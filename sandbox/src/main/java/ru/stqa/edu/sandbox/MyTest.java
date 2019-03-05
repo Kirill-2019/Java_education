f06880057125509f6caf2481bc2ab5cc89767947
Kirill-2019 <48190427+Kirill-2019@users.noreply.github.com>
2019-03-06 01:03:21 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/edu/sandbox/MyTest.java
+++ sandbox/src/main/java/ru/stqa/edu/sandbox/MyTest.java
@@ -1,11 +1,36 @@
 package ru.stqa.edu.sandbox;
 
+import java.sql.SQLOutput;
+
 public class MyTest {
 
    public static void main(String[] args) {
 
-      System.out.println("Hello, world!");
-      
+      hello("world");
+      hello("user");
+      hello("Alexei");
+
+      double l = 5;
+      System.out.println("площадь квадрата со стороной " + l + " = " + area(l));
+
+      float a = 5;
+      float b = 7;
+      System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area (a,b));
 
    }
+
+   public static void hello (String text) {
+
+      System.out.println("Hello, " + text + "!");
+   }
+
+   public static double area(double l){
+      return l*l;
+   }
+
+   public static float area (float a, float b ) {
+      return a*b;
+   }
+
 }
+
