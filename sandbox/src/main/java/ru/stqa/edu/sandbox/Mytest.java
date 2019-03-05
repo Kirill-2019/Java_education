a493fb4697311788c6568f2cef7452c7c7894fab
Kirill-2019 <48190427+Kirill-2019@users.noreply.github.com>
2019-03-05 23:23:35 +0300

    reformat

--- sandbox/src/main/java/ru/stqa/edu/sandbox/Mytest.java
+++ sandbox/src/main/java/ru/stqa/edu/sandbox/Mytest.java
@@ -2,9 +2,9 @@ package ru.stqa.edu.sandbox;
 
 public class Mytest {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-	System.out.println("Hello, world!");
+        System.out.println("Hello, world!");
 
-}
+    }
 }
